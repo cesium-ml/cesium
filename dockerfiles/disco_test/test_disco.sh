37ef1403eeea618001c55acc4f4c04dfe5ac7287
Stefan van der Walt <stefan@sun.ac.za>
2014-12-01 18:48:36 +0200

    Run command via supervisor

---
+++ dockerfiles/disco_test/test_disco.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+THIS_DIR="$( dirname "${BASH_SOURCE[0]}")"
+MLTP_DIR=$(cd "${THIS_DIR}/../.." && pwd)
+
+set -ex
+docker build -t disco_test .
+docker run -v "$MLTP_DIR:/home/mltp" disco_test
