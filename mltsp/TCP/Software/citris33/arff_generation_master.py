e40e9c939c9dbaeb3986871b3611c9a06287ba34
Stefan van der Walt <stefan@sun.ac.za>
2015-02-10 12:15:20 -0800

    Port Software/citris33

--- mltsp/TCP/Software/citris33/arff_generation_master.py
+++ mltsp/TCP/Software/citris33/arff_generation_master.py
@@ -2008,7 +2008,7 @@ def get_perc_subset(srcid_list=[], percent_list=[], niters=1, xml_dirpath='', in
             ### We generate several random, percent-subsampled vosource in order to include error info:
             #if 1:
             #    i = niters # this should just be a single (integer) subset number/iteration index
-            #for i in xrange(niters):
+            #for i in range(niters):
             for i in niters:
                 if write_multiinfo_srcids:
                     new_srcid = "%d_%2.2f_%d" % (src_id, perc, i)
@@ -2022,7 +2022,7 @@ def get_perc_subset(srcid_list=[], percent_list=[], niters=1, xml_dirpath='', in
 
                 dbi_src = db_importer.Source(make_dict_if_given_xml=False)
                 
-                for band, band_dict in gen_orig.sig.x_sdict['ts'].iteritems():
+                for band, band_dict in gen_orig.sig.x_sdict['ts'].items():
                     if ":NOMAD" in band:
                         i_start = 0
                         i_end = len(band_dict['m'])
@@ -2204,7 +2204,7 @@ ArffEngineTasks = arff_generation_master.Arff_Generation_Engine_Tasks()"""
     i_count = 0
     while n_pending > 0:
         still_pending_dict = {}
-        for engine_id, pending_result in pending_result_dict.iteritems():
+        for engine_id, pending_result in pending_result_dict.items():
             try:
                 result_val = pending_result.get_result(block=False)
             except:
