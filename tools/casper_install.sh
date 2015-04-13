21f843f4546249790e80bda560cc4e5d6e925574
Stefan van der Walt <stefanv@berkeley.edu>
2015-04-13 14:16:24 -0700

    Fix CasperJS install

--- tools/casper_install.sh
+++ tools/casper_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 THIS_DIR="$( dirname "${BASH_SOURCE[0]}")"
-EXTERNAL_DIR=$(cd "${THIS_DIR}/../external" && pwd)
-
+EXTERNAL_DIR=$(cd "${THIS_DIR}/.." && pwd)/external
 mkdir -p $EXTERNAL_DIR
+
 cd $EXTERNAL_DIR
 
 if [[ -d "casperjs" ]]; then
-    echo "CasperJS already downloaded"
+    echo "CasperJS already downloaded into $EXTERNAL_DIR"
 else
+    echo "Installing CasperJS into $EXTERNAL_DIR"
+    echo
     wget https://github.com/n1k0/casperjs/tarball/master -O - | tar -xz
     mv n1k0-casperjs-* casperjs
 fi
