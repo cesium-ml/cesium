eb9a04fd9ee1fb78602645dd5675e0dd51576cf5
Stefan van der Walt <stefanv@berkeley.edu>
2015-04-13 16:42:09 -0700

    Casper: do not specify expected number of tests

--- mltsp/tests/frontend/index.js
+++ mltsp/tests/frontend/index.js
@@ -1,4 +1,5 @@
-casper.test.begin('index loads', 1, function suite(test) {
+casper.test.begin('index loads'
+                  /*, planned nr of tests, */,  function suite(test) {
     casper.start('http://localhost:5000', function() {
         test.assertTextExists('Sign in with your Google Account',
                               'Authentication displayed on index page');
