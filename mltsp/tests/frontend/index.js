285abec25cba485a463e6dfaa30c1844736e899b
Stefan van der Walt <stefanv@berkeley.edu>
2015-04-13 15:43:12 -0700

    Add valid test for front page

--- mltsp/tests/frontend/index.js
+++ mltsp/tests/frontend/index.js
@@ -1,6 +1,7 @@
 casper.test.begin('index loads', 1, function suite(test) {
     casper.start('http://localhost:5000', function() {
-        test.assertTextExists('MLTSP', 'successfully loaded index page');
+        test.assertTextExists('Sign in with your Google Account',
+                              'Authentication displayed on index page');
     });
 
     casper.run(function() {
