7e2d056fd43d795cf350e92f5686db2f4ddf7ad2
Stefan van der Walt <stefanv@berkeley.edu>
2015-05-04 17:25:47 -0700

    base_disco: add disco test

---
+++ dockerfiles/base_disco/disco_count_words.py
@@ -0,0 +1,17 @@
+from disco.core import Job, result_iterator
+
+def map(line, params):
+    for word in line.split():
+        yield word, 1
+
+def reduce(iter, params):
+    from disco.util import kvgroup
+    for word, counts in kvgroup(sorted(iter)):
+        yield word, sum(counts)
+
+if __name__ == '__main__':
+    job = Job().run(input=["http://discoproject.org/media/text/chekhov.txt"],
+                    map=map,
+                    reduce=reduce)
+    for word, count in result_iterator(job.wait(show=True)):
+        print(word, count)
