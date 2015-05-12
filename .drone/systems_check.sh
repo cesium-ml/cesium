23e27f299e0b010c28b45722431eb3fd23fb872e
Stefan van der Walt <stefanv@berkeley.edu>
2015-05-12 13:01:24 -0700

    See if we can access /var/run/docker.sock

--- .drone/systems_check.sh
+++ .drone/systems_check.sh
@@ -5,5 +5,5 @@ echo "----------------------------------------------------"
 ls -al /*.sock
 ls -al /var/run/*.sock
 echo "Docker info:"
-docker -H unix:///docker.sock info
+docker -H unix:///var/run/docker.sock info
 echo "----------------------------------------------------"
