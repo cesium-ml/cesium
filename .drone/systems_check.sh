fb4996a692cf4a96d34715b33c321bedbbdac43f
Stefan van der Walt <stefanv@berkeley.edu>
2015-05-12 10:50:58 -0700

    Systems check: list all potential sockets

--- .drone/systems_check.sh
+++ .drone/systems_check.sh
@@ -2,7 +2,8 @@
 
 echo "Drone systems check..."
 echo "----------------------------------------------------"
-ls -al /
+ls -al /*.sock
+ls -al /var/run/*.sock
 echo "Docker info:"
 docker -H unix:///docker.sock info
 echo "----------------------------------------------------"
