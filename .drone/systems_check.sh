1618ea903730298a9622e27ada233527ac9715cd
Stefan van der Walt <stefanv@berkeley.edu>
2015-05-12 18:15:39 -0700

    Examine new shared Drone path

--- .drone/systems_check.sh
+++ .drone/systems_check.sh
@@ -6,4 +6,8 @@ echo "Current path:"
 pwd
 echo "Docker info:"
 docker -H unix:///var/run/docker.sock info
+echo "Drone shared temp:"
+ls -al /tmp/
+ls -al /tmp/drone_shared
+cat /tmp/drone_shared/*.txt
 echo "----------------------------------------------------"
