28f3e34da6f3ea5de0dafde4d5ac7dac8d49c28a
Stefan van der Walt <stefanv@berkeley.edu>
2015-05-12 18:32:18 -0700

    Move tests to shared path before executing, to facilitate mounting on inner docker container

--- .drone/build.sh
+++ .drone/build.sh
@@ -2,7 +2,24 @@
 
 set -e
 
+REPO_PATH=`pwd`
+SHARED_PATH=/tmp/`ls -1 /tmp/ | grep -E ^drone_shared | head -n 1`
+
+echo "Drone systems check..."
+echo "----------------------------------------------------"
+echo Current path: ${REPO_PATH}
+echo Shared path: ${SHARED_PATH}
+
+echo -e "Docker info:\n"
+docker -H unix:///var/run/docker.sock info
+echo
+echo "----------------------------------------------------"
+
 pip install -r requirements.txt
+
+mv ${REPO_PATH} ${SHARED_PATH}
+cd ${SHARED_PATH}/mltsp
+
 python setup.py build_ext -i
 pip install -e .
 
