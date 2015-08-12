f280a19639251c18a1f57b929663c8432699a47d
Ari Crellin-Quick <a.crellinquick@gmail.com>
2015-08-11 17:12:43 -0700

    Use RethinkDB as Celery backend

---
+++ mltsp/ext/celeryconfig.py
@@ -0,0 +1,23 @@
+#CELERY_RESULT_BACKEND = 'mltsp.ext.rethinkdb_backend:RethinkBackend'
+
+CELERY_RESULT_BACKEND = "amqp"
+
+#CELERY_RETHINKDB_BACKEND_SETTINGS = {
+    # 'host': '127.0.0.1',
+    # 'port': 28015,
+    # 'db': 'celery_test',
+    # 'auth_key': '',
+    # 'timeout': 20,
+    # 'table': 'celery_taskmeta',
+    # 'options': {}
+#}
+
+CELERY_RESULT_SERIALIZER = 'json'  # NOTE: MUST BE SET TO JSON
+
+#CELERYD_LOG_FILE = "/tmp/celery.log"
+
+CELERYD_LOG_LEVEL = "DEBUG"
+
+INSTALLED_APPS = ["mltsp"]
+
+CELERY_IMPORTS = ("mltsp", "celery_fit")
