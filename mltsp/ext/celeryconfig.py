805963899f09a26f3ab5615a53b302d39a3c112b
Ari Crellin-Quick <a.crellinquick@gmail.com>
2015-09-16 10:39:57 -0700

    Remove obsolete Celery RethinkDB backend code

--- mltsp/ext/celeryconfig.py
+++ mltsp/ext/celeryconfig.py
@@ -1,20 +1,6 @@
-#CELERY_RESULT_BACKEND = 'mltsp.ext.rethinkdb_backend:RethinkBackend'
-
 CELERY_RESULT_BACKEND = "amqp"
 
-CELERY_RETHINKDB_BACKEND_SETTINGS = {
-    'host': '127.0.0.1',
-    'port': 28015,
-    'db': 'celery_test',
-    # 'auth_key': '',
-    'timeout': 20,
-    'table': 'celery_taskmeta',
-    'options': {}
-}
-
 CELERY_RESULT_SERIALIZER = 'json'  # NOTE: MUST BE SET TO JSON
-
-#CELERYD_LOG_FILE = "/tmp/celery.log"
 
 CELERYD_LOG_LEVEL = "DEBUG"
 
