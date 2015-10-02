b999a374549b2ccdc4ca1055fedd674954a90adf
Brett Naul <brettnaul@gmail.com>
2015-10-02 11:26:25 -0700

    Remove mltsp.TCP module
    All relevant code was moved to mltsp.science_features in #70.

--- mltsp/TCP/Software/feature_extract/Code/extractors/std_extractor.py
+++
@@ -1,8 +0,0 @@
-from ..FeatureExtractor import FeatureExtractor
-from numpy import std
-
-class std_extractor(FeatureExtractor):
-    active = True
-    extname = 'std' #extractor's name
-    def extract(self):
-        return(std(self.flux_data))
