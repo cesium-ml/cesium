02e6daedd6c479c13a55889e00f1b635ca2ac3c5
Stefan van der Walt <stefanv@berkeley.edu>
2015-10-08 09:46:29 -0700

    Print build steps

--- .drone/build.sh
+++ .drone/build.sh
@@ -5,30 +5,40 @@ set -e
 REPO_PATH=`pwd`
 SHARED_PATH=/tmp/`ls -1 /tmp/ | grep -E ^drone_shared | head -n 1`
 
-echo "Drone systems check..."
-echo "----------------------------------------------------"
-echo Current path: ${REPO_PATH}
-echo Shared path: ${SHARED_PATH}
+echo "[Drone] systems check..."
+echo "[Drone] ----------------------------------------------------"
+echo "[Drone] Current path: ${REPO_PATH}"
+echo "[Drone] Shared path: ${SHARED_PATH}"
 
-echo -e "Docker info:\n"
+echo -e "[Drone] Docker info:\n"
 docker -H unix:///var/run/docker.sock info
 echo
-echo "----------------------------------------------------"
+echo "[Drone] ----------------------------------------------------"
 
+echo "[Drone] Installing requirements"
 pip install --upgrade requests six python-dateutil
 pip install -r requirements.txt
 
 mv ${REPO_PATH} ${SHARED_PATH}
 cd ${SHARED_PATH}/mltsp
 
+echo "[Drone] Build extension"
 python setup.py build_ext -i
+
+echo "[Drone] Installing mltsp in-place"
 pip install -e .
 
+echo "[Drone] Configure MLTSP"
 mltsp --install
 
+echo "[Drone] Launch RabbitMQ"
 rabbitmq-server &
 
+echo "[Drone] Launch RethinkDB"
 make db && sleep 1
+
+echo "[Drone] Initialize database"
 mltsp --init-db
 
+echo "[Drone] Run test suite"
 make test_no_docker
