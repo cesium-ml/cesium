00db541692963c943e46fa65fc5caf9465b4cf69
Ari Crellin-Quick <a.crellinquick@gmail.com>
2015-10-13 10:50:31 -0700

    Move run_script_in_container into mltsp sub-directory

--- mltsp/run_script_in_container.py
+++ mltsp/run_script_in_container.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # run_script_in_container.py
+# Will be run inside Docker container
 
 if __name__ == "__main__":
     # Run Cython setup script:
@@ -10,26 +11,16 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='MLTSP Docker scripts')
     parser.add_argument('--extract_custom_feats', action='store_true')
-    parser.add_argument('--featurize', action='store_true')
-    parser.add_argument('--build_model', action='store_true')
-    parser.add_argument('--predict', action='store_true')
     parser.add_argument('--tmp_dir', dest='tmp_dir', action='store', type=str)
     args = parser.parse_args()
 
     import sys
+    import os
     sys.path.append(args.tmp_dir)
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
     if args.extract_custom_feats:
         from mltsp.docker_scripts import docker_extract_custom_feats
         docker_extract_custom_feats.extract_custom_feats(args.tmp_dir)
-    elif args.featurize:
-        from mltsp.docker_scripts import docker_featurize
-        docker_featurize.do_featurization(args.tmp_dir)
-    elif args.build_model:
-        from mltsp.docker_scripts import docker_build_model
-        docker_build_model.build_model(args.tmp_dir)
-    elif args.predict:
-        from mltsp.docker_scripts import docker_predict
-        docker_predict.predict(args.tmp_dir)
     else:
         raise Exception("No valid script parameter passed to "
-                        "run_script_in_container.py call.")
+                        "run_script_in_container")
