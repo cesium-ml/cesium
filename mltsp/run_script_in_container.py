c7e5306ede3415838c3cf0861c3a1be8caed38ba
Ari Crellin-Quick <a.crellinquick@gmail.com>
2015-10-14 11:28:53 -0700

    Add comment explaining path append

--- mltsp/run_script_in_container.py
+++ mltsp/run_script_in_container.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     import sys
     import os
     sys.path.append(args.tmp_dir)
+    # Append mltsp package directory to path so it can be imported below
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     if args.extract_custom_feats:
         from mltsp.docker_scripts import docker_extract_custom_feats
