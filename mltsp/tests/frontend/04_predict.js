2af9556eb80ebf3df5ab3fdede382f5315d28bcf
Ari Crellin-Quick <a.crellinquick@gmail.com>
2015-10-22 12:03:40 -0700

    Remove additional unnecessary login code

--- mltsp/tests/frontend/04_predict.js
+++ mltsp/tests/frontend/04_predict.js
@@ -2,13 +2,6 @@ casper.test.begin('predict', function suite(test) {
     casper.start('http://localhost:5000', function() {
         this.page.viewportSize = { width: 1920, height: 1080 };
 
-        if(this.exists('form.login-form')){
-            this.fill('form.login-form', {
-                'login': 'testhandle@test.com',
-                'password':  'TestPass15'
-            }, true);
-        }
-
         // Predict
         casper.then(function(){
             this.click("#predictTabButton");
