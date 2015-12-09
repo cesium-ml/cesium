9725a531abf9b097dcfbbf491b814fa668db95a9
Ari Crellin-Quick <a.crellinquick@gmail.com>
2015-12-09 11:18:19 -0800

    Replace model abbreviations with more descriptive names; update test_build_model.py

--- mltsp/tests/test_build_model.py
+++ mltsp/tests/test_build_model.py
@@ -35,8 +35,9 @@ def remove_test_data():
 
 @with_setup(copy_classification_test_data, remove_test_data)
 def test_build_model_rfc():
-    """Test main model building method - RFC"""
-    build_model.create_and_pickle_model("test", "RFC", "test")
+    """Test main model building method - Random Forest Classifier"""
+    build_model.create_and_pickle_model("test", "Random Forest Classifier",
+                                        "test")
     model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
     assert hasattr(model, "predict_proba")
     assert "sklearn.ensemble.forest.RandomForestClassifier" in str(type(model))
@@ -44,8 +45,9 @@ def test_build_model_rfc():
 
 @with_setup(copy_regression_test_data, remove_test_data)
 def test_build_model_rfr():
-    """Test main model building method - RFR"""
-    build_model.create_and_pickle_model("test", "RFR", "test_reg")
+    """Test main model building method - Random Forest Regressor"""
+    build_model.create_and_pickle_model("test", "Random Forest Regressor",
+                                        "test_reg")
     model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
     assert hasattr(model, "predict")
     assert "sklearn.ensemble.forest.RandomForestRegressor" in str(type(model))
@@ -54,7 +56,7 @@ def test_build_model_rfr():
 @with_setup(copy_classification_test_data, remove_test_data)
 def test_build_model_lin_class():
     """Test main model building method - linear classifier"""
-    build_model.create_and_pickle_model("test", "LC", "test")
+    build_model.create_and_pickle_model("test", "Linear SGD Classifier", "test")
     model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
     assert hasattr(model, "predict")
     assert "SGDClassifier" in str(type(model))
@@ -63,7 +65,7 @@ def test_build_model_lin_class():
 @with_setup(copy_regression_test_data, remove_test_data)
 def test_build_model_lin_reg():
     """Test main model building method - linear regressor"""
-    build_model.create_and_pickle_model("test", "LR", "test_reg")
+    build_model.create_and_pickle_model("test", "Linear Regressor", "test_reg")
     model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
     assert hasattr(model, "predict")
     assert "LinearRegression" in str(type(model))
@@ -72,7 +74,7 @@ def test_build_model_lin_reg():
 @with_setup(copy_classification_test_data, remove_test_data)
 def test_build_model_ridge_cv():
     """Test main model building method - Ridge Classifer CV"""
-    build_model.create_and_pickle_model("test", "RC", "test")
+    build_model.create_and_pickle_model("test", "Ridge Classifier CV", "test")
     model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
     assert hasattr(model, "predict")
     assert "RidgeClassifierCV" in str(type(model))
@@ -81,10 +83,21 @@ def test_build_model_ridge_cv():
 @with_setup(copy_regression_test_data, remove_test_data)
 def test_build_model_ard_reg():
     """Test main model building method - ARD Regression"""
-    build_model.create_and_pickle_model("test", "ARDR", "test_reg")
+    build_model.create_and_pickle_model("test", "Bayesian ARD Regressor",
+                                        "test_reg")
     model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
     assert hasattr(model, "predict")
     assert "ARDRegression" in str(type(model))
+
+
+@with_setup(copy_regression_test_data, remove_test_data)
+def test_build_model_ard_reg():
+    """Test main model building method - Bayesian Ridge Regression"""
+    build_model.create_and_pickle_model("test", "Bayesian Ridge Regressor",
+                                        "test_reg")
+    model = joblib.load(pjoin(cfg.MODELS_FOLDER, "test.pkl"))
+    assert hasattr(model, "predict")
+    assert "BayesianRidge" in str(type(model))
 
 
 @with_setup(copy_classification_test_data, remove_test_data)
@@ -92,7 +105,7 @@ def test_fit_existing_model():
     """Test model building helper function."""
     featureset = xray.open_dataset(pjoin(cfg.FEATURES_FOLDER,
                                          "test_featureset.nc"))
-    model = build_model.MODELS_TYPE_DICT['RFC']()
+    model = build_model.MODELS_TYPE_DICT['Random Forest Classifier']()
     model = build_model.build_model_from_featureset(featureset, model)
     assert hasattr(model, "n_features_")
     assert hasattr(model, "predict_proba")
