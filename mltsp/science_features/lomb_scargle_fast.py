6f4beaa772e9b8b9b1de6f6a92c0b7fd00bdd5af
Brett Naul <brettnaul@gmail.com>
2016-01-21 15:00:49 -0800

    Use more sensible choice of period_range for `period_fast` feature
    Periods searched should depend only on the range of times, rather than
    the max time.

--- mltsp/science_features/lomb_scargle_fast.py
+++ mltsp/science_features/lomb_scargle_fast.py
@@ -10,7 +10,8 @@ def lomb_scargle_fast_period(t, m, e):
     and returns the estimated period 1/w. Much faster than fitting the
     full multi-frequency model used by `science_features.lomb_scargle`.
     """
-    opt_args = {'period_range': (2*t.max() / len(t), t.max()), 'quiet': True}
+    dt = t.max() - t.min()
+    opt_args = {'period_range': (2 * dt / len(t), dt), 'quiet': True}
     model = gatspy.periodic.LombScargleFast(fit_period=True, optimizer_kwds=opt_args)
     model.fit(t, m, e)
     return model.best_period
