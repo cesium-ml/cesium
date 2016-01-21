45aa37487607d0c4723b24ac528c6eaaa8563eaa
Stefan van der Walt <stefanv@berkeley.edu>
2016-01-20 16:21:59 -0800

    drone: use virtual env

--- .drone/build.sh
+++ .drone/build.sh
@@ -10,6 +10,11 @@ echo "[Drone] ----------------------------------------------------"
 echo "[Drone] Current path: ${REPO_PATH}"
 echo "[Drone] ----------------------------------------------------"
 
+echo "[Drone] Creating Python virtual environment"
+pip install virtualenv
+virtualenv /envs/python2.7 -p python2.7
+source /envs/python2.7/bin/activate
+
 echo "[Drone] Installing base requirements"
 pip install --upgrade pip requests six python-dateutil nose nose-exclude
 hash -d pip  # find upgraded pip
