d5fcd88e105953c425fd788828255bced5018aa1
Stefan van der Walt <stefanv@berkeley.edu>
2016-01-26 17:51:04 -0800

    Wait for supervisord to be up before launching frontend tests
    This is done by configuring supervisord to launch a monitor server, accessible
    via supervisorctl.  By monitoring the output of supervisorctl, we can figure
    out when all services have entered the 'RUNNING' state, and launch the
    javascript test suite.

--- tools/casper_tests.py
+++ tools/casper_tests.py
@@ -5,22 +5,52 @@ import subprocess
 import glob
 import os
 import sys
+import time
 
 
-if __name__ == "__main__":
+web_client_path = os.path.join(os.path.dirname(__file__), '../web_client')
+
+
+def supervisor_status():
+    """
+    Return output from supervisorctl
+    """
+    return subprocess.check_output(['supervisorctl', 'status'],
+                                   cwd=web_client_path).split(b'\n')[:-1]
+
+
+def reset_db():
+    """
+    Re-initialize all tables in the test database
+    """
     os.environ["MLTSP_TEST_DB"] = "1"
     from mltsp.Flask import flask_app
     flask_app.db_init(force=True)
-    from mltsp.Flask.flask_app import app
 
-    p = multiprocessing.Process(target=app.run)
-    p.start()
 
+if __name__ == '__main__':
+    reset_db()
+
+    web_client = subprocess.Popen('make', cwd=web_client_path)
+
+    print('[test_frontend] Waiting for supervisord to launch all server processes...')
+
+    timeout = 0
+    while (timeout < 5) and
+          (not all([b'RUNNING' in line for line in supervisor_status()])):
+        time.sleep(1)
+        timeout += 1
+
+    if timeout == 5:
+        print('[test_frontend] Could not launch server processes; terminating')
+        sys.exit(-1)
+
+    print('[test_frontend] Launching CasperJS...')
     tests = sorted(glob.glob('mltsp/tests/frontend/*.js'))
-    status = subprocess.call(['external/casperjs/bin/casperjs', '--verbose',
-                              '--log-level=debug', 'test'] + tests)
+    subprocess.call(['external/casperjs/bin/casperjs', '--verbose',
+                     '--log-level=debug', 'test'] + tests)
 
-    p.terminate()
+    print('[test_frontend] Terminating supervisord...')
+    web_client.terminate()
 
     sys.exit(status)
-
