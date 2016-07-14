1381e0c7ddb2a405bff63663f0ce5dcd2f0f70a0
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-07-14 09:56:04 -0700

    Add LOMB_SCARGLE_FEATURES_LIST to science features tools

--- cesium/science_feature_tools.py
+++ cesium/science_feature_tools.py
@@ -3,7 +3,8 @@ import dask.async
 from . import science_features as sf
 
 
-__all__ = ['FEATURES_LIST', 'generate_science_features']
+__all__ = ['FEATURES_LIST', 'LOMB_SCARGLE_FEATURES_LIST',
+           'generate_science_features']
 
 
 FEATURES_LIST = [
@@ -77,6 +78,62 @@ FEATURES_LIST = [
     'stetson_j',
     'stetson_k',
     'weighted_average']
+
+LOMB_SCARGLE_FEATURES_LIST = [
+    'fold2P_slope_10percentile',
+    'fold2P_slope_90percentile',
+    'freq1_amplitude1',
+    'freq1_amplitude2',
+    'freq1_amplitude3',
+    'freq1_amplitude4',
+    'freq1_freq',
+    'freq1_lambda',
+    'freq1_rel_phase1',
+    'freq1_rel_phase2',
+    'freq1_rel_phase3',
+    'freq1_rel_phase4',
+    'freq1_signif',
+    'freq2_amplitude1',
+    'freq2_amplitude2',
+    'freq2_amplitude3',
+    'freq2_amplitude4',
+    'freq2_freq',
+    'freq2_rel_phase1',
+    'freq2_rel_phase2',
+    'freq2_rel_phase3',
+    'freq2_rel_phase4',
+    'freq3_amplitude1',
+    'freq3_amplitude2',
+    'freq3_amplitude3',
+    'freq3_amplitude4',
+    'freq3_freq',
+    'freq3_rel_phase1',
+    'freq3_rel_phase2',
+    'freq3_rel_phase3',
+    'freq3_rel_phase4',
+    'freq_amplitude_ratio_21',
+    'freq_amplitude_ratio_31',
+    'freq_frequency_ratio_21',
+    'freq_frequency_ratio_31',
+    'freq_model_max_delta_mags',
+    'freq_model_min_delta_mags',
+    'freq_model_phi1_phi2',
+    'freq_n_alias',
+    'freq_signif_ratio_21',
+    'freq_signif_ratio_31',
+    'freq_varrat',
+    'freq_y_offset',
+    'linear_trend',
+    'lomb_model',
+    'medperc90_2p_p',
+    'p2p_model',
+    'p2p_scatter_2praw',
+    'p2p_scatter_over_mad',
+    'p2p_scatter_pfold_over_mad',
+    'p2p_ssqr_diff_over_var',
+    'period_folded_model',
+    'periodic_model',
+    'scatter_res_raw']
 
 
 def generate_science_features(t, m, e, features_to_compute=FEATURES_LIST):
