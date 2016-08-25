517cd182a39304cd404b80017122dda12d48eedc
Brett Naul <brettnaul@gmail.com>
2016-08-25 16:59:51 -0700

    Fix version of matplotlib in doc build to supress warning

--- .drone/travis_script.sh
+++ .drone/travis_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 section "Tests"
 
@@ -10,6 +10,18 @@ section_end "Tests"
 
 section "Build.docs"
 
+source ~/virtualenv/python3.5/bin/activate
+pip install matplotlib
+ls ~/.cache/matplotlib
+fc-list
+python -c 'import matplotlib; print(matplotlib.get_cachedir())'
+python -c 'import matplotlib.pyplot'
+python -c 'import matplotlib.pyplot'
+find ~ -name fontList.cache
+find ~ -name fontList.py3k.cache
+ls ~/.cache/matplotlib
+ls ~/.cache/fontconfig
+ls ~/.matplotlib
 make html
 
 section_end "Build.docs"
