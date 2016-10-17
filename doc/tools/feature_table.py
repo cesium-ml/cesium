6a5a46be53a167a140b543b14a44a81ce0896a96
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-10-17 14:01:57 -0700

    Add features.graphs.extra_feature_docs dict for missing feature docstrings

--- doc/tools/feature_table.py
+++ doc/tools/feature_table.py
@@ -1,7 +1,7 @@
 from tabulate import tabulate
 import re
 from cesium.features.graphs import (CADENCE_GRAPH, GENERAL_GRAPH,
-                                    LOMB_SCARGLE_GRAPH)
+                                    LOMB_SCARGLE_GRAPH, extra_feature_docs)
 
 
 def feature_graph_to_rst_table(graph, category_name):
@@ -11,17 +11,23 @@ def feature_graph_to_rst_table(graph, category_name):
     for feature_name in sorted(
             graph, key=lambda s: [int(t) if t.isdigit() else t
                                   for t in re.split('(\d+)', s)]):
-        table.append([feature_name, graph[feature_name][0].__doc__.split('\n')[0]])
+        description = (extra_feature_docs[feature_name] if feature_name in
+                       extra_feature_docs else
+                       ' '.join([e.strip() for e in
+                                 graph[feature_name][0].__doc__.split('\n\n')[0]
+                                 .strip().split('\n')]))
+        table.append([feature_name, description])
 
     return tabulate(table, headers=header, tablefmt='rst')
 
 
 def write_feature_tables():
     with open('../features.rst', 'w') as f:
-        f.write('================\n'
-                'Cesium Features\n'
-                '================\n\n')
+        f.write('==============================\n'
+                'Cesium Features - By Category\n'
+                '==============================\n\n')
         for graph, category in [[CADENCE_GRAPH, 'Cadence/Error'],
                                 [GENERAL_GRAPH, 'General'],
                                 [LOMB_SCARGLE_GRAPH, 'LOMB_SCARGLE']]:
-            f.write(feature_graph_to_rst_table(graph, category) + '\n')
+            f.write(feature_graph_to_rst_table(graph, category) + '\n\n')
+
