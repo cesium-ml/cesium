d16b80065b01e23452020b6405a54b82d4eca8f0
Ananth Ravi Kumar <ananthravikumar@berkeley.edu>
2016-10-20 19:40:28 -0700

    Add code coverage

--- .drone/travis_script.sh
+++ .drone/travis_script.sh
@@ -7,10 +7,12 @@ source ~/envs/cesium/bin/activate
 
 section "Tests"
 
-make ${TEST_TARGET}
+if [[ $COVERAGE==1 ]]; then
+    NOSE_FLAGS='--with-coverage'
+fi
+nosetests -v --exe $NOSE_FLAGS
 
 section_end "Tests"
-
 
 section "Build.docs"
 
