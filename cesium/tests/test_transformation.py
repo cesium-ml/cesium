46f9f7a13741c3d5c79d11cf4b182c24ea434609
Ananth Ravi Kumar <ananthravikumar@berkeley.edu>
2016-10-24 13:12:33 -0700

    added more to test_transformation, coverage now at 83%

--- cesium/tests/test_transformation.py
+++ cesium/tests/test_transformation.py
@@ -44,3 +44,29 @@ def test_transform_ts_files():
 
 def test_train_test_split_basecase():
     npt.assert_equal(transformation.train_test_split([]), ([], []))
+
+def test_train_test_split_ratios():
+    n_class1 = 4
+    n_class2 = 8
+    transform_type = "Train/Test Split"
+    time_series = []
+    for i in range(n_class1):
+        t, m, e = sample_values()
+        time_series.append(TimeSeries(t, m, e, target='class1'))
+    for i in range(n_class2):
+        t, m, e = sample_values()
+        time_series.append(TimeSeries(t, m, e, target='class2'))
+    outputs = transformation.train_test_split(time_series, test_size = 0.5, train_size= 0.5)
+    npt.assert_equal(len(outputs[1]), len(time_series)/2)
+    npt.assert_equal(len(outputs[0]), len(time_series)/2)
+
+    # testing with different ratios
+    outputs1 = transformation.train_test_split(time_series, test_size = 0.7, train_size= 0.3)
+    outputs2 = transformation.train_test_split(time_series, test_size = 0.8, train_size= 0.2)
+    outputs3 = transformation.train_test_split(time_series, test_size = 0.6, train_size= 0.4)
+    npt.assert_equal(len(outputs1[1]), len(time_series)*0.7)
+    npt.assert_equal(len(outputs1[0]), len(time_series)*0.3)
+    npt.assert_equal(len(outputs2[1]), len(time_series)*0.8)
+    npt.assert_equal(len(outputs2[0]), len(time_series)*0.2)
+    npt.assert_equal(len(outputs3[1]), len(time_series)*0.6)
+    npt.assert_equal(len(outputs3[0]), len(time_series)*0.4)
