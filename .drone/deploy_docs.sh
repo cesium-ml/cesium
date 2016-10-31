db4ec6708fb7325e4674dc6fbe19b22de1c288ce
Jie <jiesun@berkeley.edu>
2016-10-31 15:10:30 -0700

    switched to doctr

--- .drone/deploy_docs.sh
+++ .drone/deploy_docs.sh
@@ -2,26 +2,13 @@
 
 set -e
 
-if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == "master" &&
-      $DEPLOY_DOCS == 1 && -n "$GH_TOKEN" ]]
+source ~/envs/cesium/bin/activate
+
+if [[ $DEPLOY_DOCS == 1 ]]
 then
-    # See https://help.github.com/articles/creating-an-access-token-for-command-line-use/ for how to generate a token
-    # See http://docs.travis-ci.com/user/encryption-keys/ for how to generate
-    # a secure variable on Travis
-    echo "-- pushing docs --"
-
-    (
-    git clone --quiet --branch=gh-pages https://${GH_REF} doc_build
-    cp -r doc/_build/html/* doc_build/
-
-    cd doc_build
-    git config user.email "travis@travis-ci.com"
-    git config user.name "Travis Bot"
-
-    git add -A
-    git commit -m "Deployed to GitHub Pages"
-    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
-    )
+    pip install doctr
+    doctr deploy --gh-pages-docs "/" --deploy-repo "cesium-ml/docs"
 else
     echo "-- will only push docs from master --"
 fi
+
