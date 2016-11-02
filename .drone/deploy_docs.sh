90d5a57303b58860dede8a75d9848514d1965659
Brett Naul <brettnaul@gmail.com>
2016-11-01 20:46:09 -0700

    Fix doctr path to docs

--- .drone/deploy_docs.sh
+++ .drone/deploy_docs.sh
@@ -7,7 +7,7 @@ source ~/envs/cesium/bin/activate
 if [[ $DEPLOY_DOCS == 1 ]]
 then
     pip install doctr
-    doctr deploy --gh-pages-docs "/" --deploy-repo "cesium-ml/docs"
+    doctr deploy --gh-pages-docs '.' --deploy-repo "cesium-ml/docs"
 else
     echo "-- will only push docs from master --"
 fi
