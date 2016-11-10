848cc919bdc2856220db0df2873158acc65bc486
Ananth Ravi Kumar <ananthravikumar@berkeley.edu>
2016-11-09 18:03:58 -0800

    Testcoverage (#215)
    Improve code coverage on test_cadence_features and test_transformation

--- cesium/features/tests/test_cadence_features.py
+++ cesium/features/tests/test_cadence_features.py
@@ -10,9 +10,9 @@ def test_delta_t_hist():
     times, values, errors = irregular_random()
     delta_ts = [pair[1] - pair[0] for pair in itertools.combinations(times, 2)]
     nbins = 50
-    bins = np.linspace(0, max(times) - min(times), nbins+1)
+    bins = np.linspace(0, max(times) - min(times), nbins + 1)
     npt.assert_allclose(cf.delta_t_hist(times, nbins), np.histogram(delta_ts,
-        bins=bins)[0])
+                                                                    bins=bins)[0])
 
 
 def test_normalize_hist():
@@ -20,26 +20,47 @@ def test_normalize_hist():
     times, values, errors = irregular_random()
     delta_ts = [pair[1] - pair[0] for pair in itertools.combinations(times, 2)]
     nbins = 50
-    bins = np.linspace(0, max(times) - min(times), nbins+1)
+    bins = np.linspace(0, max(times) - min(times), nbins + 1)
     nhist = cf.normalize_hist(cf.delta_t_hist(times, nbins), max(times) -
-                               min(times))
+                              min(times))
     npt.assert_allclose(nhist, np.histogram(delta_ts,
-        bins=bins, density=True)[0])
+                                            bins=bins, density=True)[0])
 
 
 def test_find_sorted_peaks():
     """Test peak-finding algorithm."""
-    x = np.array([0,5,3,1]) # Single peak
-    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[1,5]]))
+    x = np.array([0, 5, 3, 1])  # Single peak
+    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[1, 5]]))
 
-    x = np.array([0,5,3,6,1]) # Multiple peaks
-    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[3,6],[1,5]]))
+    x = np.array([0, 5, 3, 6, 1])  # Multiple peaks
+    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[3, 6], [1, 5]]))
 
-    x = np.array([3,1,3]) # End-points can be peaks
-    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[0,3],[2,3]]))
+    x = np.array([3, 1, 3])  # End-points can be peaks
+    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[0, 3], [2, 3]]))
 
-    x = np.array([0,3,3,3,0]) # In case of ties, peak is left-most point
-    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[1,3]]))
+    x = np.array([0, 3, 3, 3, 0])  # In case of ties, peak is left-most point
+    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[1, 3]]))
 
-    x = np.array([0,3,3,5,0]) # Tie is a peak only if greater than next value
-    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[3,5]]))
+    # Tie is a peak only if greater than next value
+    x = np.array([0, 3, 3, 5, 0])
+    npt.assert_allclose(cf.find_sorted_peaks(x), np.array([[3, 5]]))
+
+
+def test_peak_ratio():
+    """ Test peak ratio method."""
+    x = np.array([0, 5, 2, 3, 1])
+    peaks1 = cf.find_sorted_peaks(x)
+    npt.assert_almost_equal(cf.peak_ratio(peaks1, 0, 1), 5 / 3)
+    assert cf.peak_ratio(peaks1, 1, 6) is None
+    peaks2 = []
+    assert cf.peak_ratio(peaks2, 1, 2) is None
+
+
+def test_peak_bins():
+    """ Test peak bins method """
+    x = np.array([0, 5, 2, 3, 1])
+    peaks1 = cf.find_sorted_peaks(x)
+    npt.assert_almost_equal(cf.peak_bin(peaks1, 0), 1)
+    npt.assert_almost_equal(cf.peak_bin(peaks1, 1), 3)
+    result1 = cf.peak_bin(peaks1, 6)
+    assert cf.peak_bin(peaks1, 6) is None
