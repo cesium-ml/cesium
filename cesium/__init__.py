0d413a002791cac981025e273caa351a0ad15515
Brett Naul <brettnaul@gmail.com>
2016-11-10 15:16:32 -0800

    Add submodules to `cesium`

--- cesium/__init__.py
+++ cesium/__init__.py
@@ -4,3 +4,5 @@ See http://cesium.ml for more information.
 """
 
 from .version import version as __version__
+from . import (build_model, data_management, featurize, features, predict,
+               time_series, transformation)
