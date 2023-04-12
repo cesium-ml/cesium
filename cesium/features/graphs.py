e99b6d055083fe041eb0c311c208ba96ad1e906b
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2023-04-12 19:28:34 -0400

    added time_lag_skew_kurtosis and time_lag_stats

--- cesium/features/graphs.py
+++ cesium/features/graphs.py
@@ -8,6 +8,8 @@ from .cadence_features import (
     find_sorted_peaks,
     peak_bin,
     peak_ratio,
+    compute_time_lag_stats,
+    compute_time_lag_skew_kurtosis,
 )
 
 from .common_functions import (
@@ -144,6 +146,8 @@ feature_categories = {
         "stetson_j",
         "stetson_k",
         "weighted_average",
+        "time_lag_stats",
+        "time_lag_skew_kurtosis"
     ],
     "Lomb-Scargle (Periodic)": [
         "fold2P_slope_10percentile",
@@ -272,6 +276,8 @@ dask_feature_graph = {
     "stetson_j": (stetson_j, "m"),
     "stetson_k": (stetson_k, "m"),
     "weighted_average": (weighted_average, "m", "e"),
+    "time_lag_stats": (compute_time_lag_stats, "cads"),
+    "time_lag_skew_kurtosis": (compute_time_lag_skew_kurtosis, "cads"),
     # QSO model features
     "qso_model": (qso_fit, "t", "m", "e"),
     "qso_log_chi2_qsonu": (get_qso_log_chi2_qsonu, "qso_model"),
